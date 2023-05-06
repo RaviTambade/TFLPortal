6e936ea56a8ca4fea38f3cdfd7379d7d77f82d18
unknown <rushikeshchikane101@gmail.com>
2023-05-06 20:08:06 +0530

    Template Upgrade

--- light-bootstrap-dashboard-angular2-master/src/app/sidebar/sidebar.component.ts
+++ light-bootstrap-dashboard-angular2-master/src/app/sidebar/sidebar.component.ts
@@ -8,9 +8,9 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+    { path: '/dashboard', title: 'Home',  icon: 'pe-7s-graph', class: '' },
     { path: '/TimeSheet', title: 'Time Sheet',  icon:'pe-7s-user', class: '' },
-    { path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
+    { path: '/table', title: 'Tables',  icon:'pe-7s-note2', class: '' },
     { path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
     { path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
     { path: '/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
