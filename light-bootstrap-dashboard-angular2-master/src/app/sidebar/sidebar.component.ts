763d9d5eec0343c7fd1fbcfd7cba7b234d1499cc
unknown <rushikeshchikane101@gmail.com>
2023-05-06 20:58:20 +0530

    View Changes

--- light-bootstrap-dashboard-angular2-master/src/app/sidebar/sidebar.component.ts
+++ light-bootstrap-dashboard-angular2-master/src/app/sidebar/sidebar.component.ts
@@ -31,7 +31,7 @@ export class SidebarComponent implements OnInit {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
   isMobileMenu() {
-      if ($(window).width() > 1200) {
+      if ($(window).width() > 991) {
           return false;
       }
       return true;
