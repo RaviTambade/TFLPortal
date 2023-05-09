41e0e5c7abb65947f10fa95e6726fe1a18c58aff
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-05-09 12:54:20 -0700

    Changes

--- Userservices/Program.cs
+++ Userservices/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 builder.Services.AddTransient<IUserRepository,UserRepository>();
 builder.Services.AddTransient<IUserServices,UserService>();
@@ -27,7 +28,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader());
 
 app.UseAuthorization();
 
