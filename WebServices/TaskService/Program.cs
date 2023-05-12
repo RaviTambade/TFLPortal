9d56853b7ffff914197ec6d0abd89fb2a0d58504
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2023-05-12 17:53:13 +0530

    taskprogramfilechanges

--- WebServices/TaskService/Program.cs
+++ WebServices/TaskService/Program.cs
@@ -1,19 +1,14 @@
 using PMS.Repositories;
-using PMS.Repositories.Interfaces;
+using  PMS.Repositories.Interfaces;
 using PMS.Services;
 using PMS.Services.Interfaces;
-
-
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddCors();
-
 
 builder.Services.AddTransient<ITaskRepository,TaskRepository>();
 builder.Services.AddTransient<ITaskServices,TaskServices>();
@@ -21,6 +16,8 @@ builder.Services.AddTransient<ITaskServices,TaskServices>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
@@ -31,14 +28,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader());
 
 app.UseAuthorization();
-
-
-app.UseCors(x => x.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader());
 
 app.MapControllers();
 
