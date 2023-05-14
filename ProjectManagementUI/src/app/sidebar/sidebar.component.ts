ab2b0b3616068ff52d584fd8aa1e883588817f15
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-05-14 10:08:35 +0530

    Updated

--- ProjectManagementUI/src/app/sidebar/sidebar.component.ts
+++ ProjectManagementUI/src/app/sidebar/sidebar.component.ts
@@ -13,14 +13,14 @@ export const ROUTES: RouteInfo[] = [
     //{ path: '/table', title: 'Tables',  icon:'pe-7s-note2', class: '' },
     
     //{ path: '/login', title: 'login',  icon:'pe-7s-news-paper', class: '' },
-    //{ path: '/register', title: 'Register',  icon:'pe-7s-science', class: '' },
+    { path: '/register', title: 'Register',  icon:'pe-7s-science', class: '' },
 
     // { path: '/get-employees', title: 'All Employees',  icon:'pe-7s-study', class: '' },
     // { path: '/insert-employees', title: 'Add Employees',  icon:'pe-7s-study', class: '' },
     // { path: '/employeedetails', title: 'Employee Details',  icon:'pe-7s-study', class: '' },
     // { path: '/updateemployee', title: 'Update Employee',  icon:'pe-7s-study', class: '' },
    
-    
+
     { path: '/getall-projects', title: 'Projects List',  icon:'pe-7s-study', class: '' },
     { path: '/insert-prject', title: 'Add Project',  icon:'pe-7s-study', class: '' },
     { path: '/projectdetails', title: 'Project Details',  icon:'pe-7s-study', class: '' },
