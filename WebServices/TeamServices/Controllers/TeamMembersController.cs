fffccceee466cb415aeb083c35aaad14948bdeb3
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2023-05-18 22:43:05 +0530

    teammeberwebapi

---
+++ WebServices/TeamServices/Controllers/TeamMembersController.cs
@@ -0,0 +1,72 @@
+using PMS.Models;
+using PMS.Services.Interfaces;
+using PMS.AspNetCore.Mvc;
+
+namespace TeamMember.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class TeamMemberController : ControllerBase
+{
+
+    private readonly ITeamService _service;
+
+    public TeamsController(ITeamService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    [Route("getall")]
+    public IEnumerable<Team> GetAllTeams()
+    {
+
+        List<Team> teams = _service.GetAll();
+
+        return teams;
+
+    }
+
+    [HttpGet]
+    [Route("getbyid/{id}")]
+    public Team GetById(int id)
+    {
+        Team team = _service.GetById(id);
+
+
+        return team;
+    }
+
+    [HttpPost]
+    [Route("InsertTeam")]
+    public bool InsertTeam(Team team)
+    {
+        bool status = _service.Insert(team);
+
+
+        return status;
+    }
+
+    [HttpPut]
+    [Route("updateTeam/{id}")]
+
+    public bool UpdateTeam(Team team)
+    {
+        bool status = _service.Update(team);
+
+        return status;
+    }
+
+
+    [HttpDelete]
+    [Route("DeleteTeam/{id}")]
+    public bool DeleteTeam(int id)
+    {
+        bool status = _service.Delete(id);
+
+        return status;
+    }
+
+
+   
+}
---
+++ WebServices/TeamServices/Models/TeamMember.cs
@@ -0,0 +1,10 @@
+using System;
+namespace PMS.Models;
+public class TeamMember
+{
+    public int TeamId{ get; set; }
+    
+    public int EmpId{ get; set; }
+
+    public int RoleId{ get; set; }
+}
