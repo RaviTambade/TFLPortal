19d3c26b116c10e9894154a4e60298e71346c086
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-05-18 22:34:34 +0530

    Database Update

--- DataBase/ProjectManagement.sql
+++ DataBase/ProjectManagement.sql
@@ -1,15 +1,13 @@
-
 CREATE DATABASE PMS;
 USE PMS;
 
+
 CREATE TABLE users(user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(50) UNIQUE NOT NULL,contact_number VARCHAR(20), password VARCHAR(50) NOT NULL);
 CREATE TABLE accounts(account_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,account_number VARCHAR(25) UNIQUE, ifsc_code VARCHAR(50) ,register_date DATETIME ,balance DOUBLE);
-CREATE TABLE employees(emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, empfirst_name VARCHAR (50),emplast_name VARCHAR(50),birth_date DATETIME,hire_date DATETIME,contact_number VARCHAR(20),email VARCHAR(50) UNIQUE NOT NULL,password varchar(50)NOT NULL,account_number VARCHAR(25) NOT NULL UNIQUE ,CONSTRAINT fk_account_no1 FOREIGN KEY(account_number) REFERENCES accounts(account_number) ON UPDATE CASCADE ON DELETE CASCADE); 
+CREATE TABLE employees(emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, empfirst_name VARCHAR (50),emplast_name VARCHAR(50),birth_date DATETIME,hire_date DATETIME,contact_number VARCHAR(20),account_number VARCHAR(25) NOT NULL UNIQUE ,CONSTRAINT fk_account_no1 FOREIGN KEY(account_number) REFERENCES accounts(account_number) ON UPDATE CASCADE ON DELETE CASCADE, user_id int NOT NULL UNIQUE ,CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE); 
 CREATE TABLE team(team_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, team_name VARCHAR (50)UNIQUE);
 CREATE TABLE role(role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,role_name varchar (50));
-CREATE TABLE team_member(team_member_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, team_id INT NOT NULL, CONSTRAINT fk_teamid FOREIGN KEY (team_id) REFERENCES team (team_id)ON UPDATE CASCADE  ON DELETE CASCADE ,emp_id INT NOT NULL, CONSTRAINT fk_empid FOREIGN KEY (emp_id) REFERENCES employees(emp_id)ON UPDATE CASCADE  ON DELETE CASCADE ,role_id INT NOT NULL, CONSTRAINT fk_roleid FOREIGN KEY (role_id) REFERENCES role(role_id)ON UPDATE CASCADE  ON DELETE CASCADE );
-CREATE TABLE project(proj_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,proj_name VARCHAR(100),planned_startDate DATETIME,planend_endDate DATETIME,actual_startDate DATETIME NOT NULL,actual_endDate DATETIME NOT NULL,proj_desc VARCHAR(255));
-CREATE TABLE project_manager(pm_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,proj_id INT NOT NULL, CONSTRAINT fk_proid FOREIGN KEY (proj_id) REFERENCES project(proj_id)ON UPDATE CASCADE  ON DELETE CASCADE,user_id INT NOT NULL, CONSTRAINT fk_userid FOREIGN KEY (user_id) REFERENCES users(user_id)ON UPDATE CASCADE  ON DELETE CASCADE);
+CREATE TABLE project(proj_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,proj_name VARCHAR(100),startDate DATETIME NOT NULL,endDate DATETIME NOT NULL,proj_desc VARCHAR(255),  emp_id INT NOT NULL,CONSTRAINT fk_emp_id FOREIGN KEY(emp_id) REFERENCES employees(emp_id) ON UPDATE CASCADE ON DELETE CASCADE);
 CREATE TABLE client(client_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,clt_name VARCHAR(50),clt_address VARCHAR(200),clt_details VARCHAR(200),account_number VARCHAR(25) NOT NULL UNIQUE ,CONSTRAINT fk_account_no2 FOREIGN KEY(account_number) REFERENCES accounts(account_number) ON UPDATE CASCADE ON DELETE CASCADE);
 CREATE TABLE on_project(on_project_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,proj_id INT NOT NULL, CONSTRAINT fk_projid FOREIGN KEY (proj_id) REFERENCES project(proj_id)ON UPDATE CASCADE  ON DELETE CASCADE,client_id INT NOT NULL, CONSTRAINT fk_clientid FOREIGN KEY (client_id) REFERENCES client(client_id)ON UPDATE CASCADE  ON DELETE CASCADE);
 CREATE TABLE task(task_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,task_name VARCHAR(50),proj_id INT NOT NULL, CONSTRAINT fk_proidd FOREIGN KEY (proj_id) REFERENCES project(proj_id)ON UPDATE CASCADE  ON DELETE CASCADE,description VARCHAR(200),start_date DATETIME,end_date DATETIME);
@@ -37,6 +35,7 @@ BEGIN
     SELECT user_id INTO userId FROM users where email=NEW.email;
 END //
 DELIMITER ;
+
 INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1001','MAHB0000286','2023-03-01 12:40:40',22555);
 INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1002','MAHB0000286','2023-03-01 12:40:40',22555);
 INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1003','MAHB0000286','2023-03-01 12:40:40',22555);
@@ -56,6 +55,10 @@ INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '10
 INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1017','AXIS0000285','2021-05-02 07-40-35',232500);
 INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1018','MAHB0000284','2021-08-03 07-40-35',235250);
 INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1019','UBIN0000286','2021-08-04 07-40-35',23520);
+INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1020','UBIN0000286','2021-08-04 07-40-35',25000);
+INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1021','UBIN0000286','2021-08-04 07-40-35',25000);
+INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1022','UBIN0000286','2021-08-04 07-40-35',25000);
+INSERT INTO accounts(account_number,ifsc_code,register_date,balance) VALUES( '1027','UBIN0000286','2021-08-04 07-40-35',25000);
 
 
 INSERT INTO employees(empfirst_name,emplast_name,birth_date,hire_date,contact_number,email,password,account_number) VALUES('Rushikesh','Chikane','1998-05-19','2023-02-01','7038548505','Rushi@12345','RC@12345','1001');
@@ -75,11 +78,26 @@ INSERT INTO employees(empfirst_name,emplast_name,birth_date,hire_date,contact_nu
 INSERT INTO payrollCycles(payroll_cycle_year,payroll_cycle_number,start_date,end_date,deposit_date)VALUES('2022-05-19',12,'2022-05-19','2023-05-19','2022-05-25');
 INSERT INTO payrollCycles(payroll_cycle_year,payroll_cycle_number,start_date,end_date,deposit_date)VALUES('2022-04-22',12,'2022-05-15','2023-05-22','2022-05-16');
 INSERT INTO payrollCycles(payroll_cycle_year,payroll_cycle_number,start_date,end_date,deposit_date)VALUES('2022-05-19',12,'2022-05-19','2023-05-19','2022-05-25');         
+      
 
-INSERT INTO project(proj_name,planned_startDate,planend_endDate,actual_startDate,actual_endDate,proj_desc)VALUES('Online Meeting Sheduling','2021-02-01','2021-03-01','2021-02-02','2021-03-03','Compeny requirement want to organize meetins online');
-INSERT INTO project(proj_name,planned_startDate,planend_endDate,actual_startDate,actual_endDate,proj_desc)VALUES('Online Interview Sheduling','2022-05-10','2022-05-12','2022-05-10','2022-05-13','We want to argent hiring of new employeess for new projects');
 
-         
+INSERT INTO team(team_name)VALUES('alpha-1');
+INSERT INTO team(team_name)VALUES('alpha-2');
+INSERT INTO team(team_name)VALUES('alpha-3');
+
+INSERT INTO role(role_name)VALUES('Developer');
+INSERT INTO role(role_name)VALUES('Consultant');
+INSERT INTO role(role_name)VALUES('Tester');
+INSERT INTO role(role_name)VALUES('Manager');
+
+
+
+INSERT INTO project(proj_name,planned_startDate,planend_endDate,actual_startDate,actual_endDate,proj_desc,team_member_id)VALUES('Meeting Sheduling','2021-02-01','2021-03-01','2021-02-02','2021-03-03','Compeny requirement want to organize meetins online',1);
+INSERT INTO project(proj_name,planned_startDate,planend_endDate,actual_startDate,actual_endDate,proj_desc,team_member_id)VALUES('Interview Sheduling','2022-05-10','2022-05-12','2022-05-10','2022-05-13','We want to argent hiring of new employeess for new projects',2);
+INSERT INTO project(proj_name,planned_startDate,planend_endDate,actual_startDate,actual_endDate,proj_desc,team_member_id)VALUES('Audit Sheduling On Testing','2022-03-18','2021-04-09','2020-07-12','2021-10-16','We want to Aodit Our Testing Process',13);
+INSERT INTO project(proj_name,planned_startDate,planend_endDate,actual_startDate,actual_endDate,proj_desc,team_member_id)VALUES('Audit Shedulin on Coading','2020-01-19','2020-01-10','2020-09-15','2020-06-10','We want to Audit our Coading rocess',15);
+  
+
 INSERT INTO timesheets(start_date,week1_monday,week1_tuesday,week1_wednesday,week1_thursday,week1_friday,week1_saturday, week1_sunday,
                                 week2_monday,week2_tuesday,week2_wednesday,week2_thursday,week2_friday,week2_saturday, week2_sunday,
                                 week3_monday,week3_tuesday,week3_wednesday,week3_thursday,week3_friday,week3_saturday, week3_sunday,
@@ -99,38 +117,8 @@ INSERT INTO timesheets(start_date,week1_monday,week1_tuesday,week1_wednesday,wee
                                      '3:05:24','4:35:23','3:15:43','6:25:28','7:55:19','4:12:25','6:07:05',
                                      '5:05:27','5:35:42','4:17:47','5:15:55','5:06:19','5:55:25','3:18:55',
                                      '1:05:54','5:45:33','6:12:43','4:55:27','6:42:19','6:17:25','4:18:29',
-                                      1,1,1,'remaining work in process');
+                                      1,1,1,'remaining work in process');  
 
-INSERT INTO team(team_name)VALUES('alpha-1');
-INSERT INTO team(team_name)VALUES('alpha-2');
-INSERT INTO team(team_name)VALUES('alpha-3');
-
-INSERT INTO role(role_name)VALUES('Developer');
-INSERT INTO role(role_name)VALUES('Consultant');
-INSERT INTO role(role_name)VALUES('Tester');
-INSERT INTO role(role_name)VALUES('Manager');
-
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(1,1,1);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(1,2,1);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(1,3,3);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(1,4,3);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(1,13,2);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(1,11,4);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(2,5,1);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(2,6,1);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(2,7,3);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(2,8,3);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(2,12,4);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(3,1,1);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(3,5,1);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(3,7,3);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(3,8,3);
-INSERT INTO team_member(team_id,emp_id,role_id)VALUES(3,10,4);
-
-
-
-INSERT INTO project_manager(proj_id,user_id)VALUES(1,11);
-INSERT INTO project_manager(proj_id,user_id)VALUES(2,12);
 
 
 INSERT INTO client(clt_name,clt_address,clt_details,account_number)VALUES('Vishwambhar Kapare','Pune RajguruNagar','Client want to create online meeting portal for their compeny','1001');
@@ -143,12 +131,14 @@ INSERT INTO task(task_name,proj_id,description,start_date,end_date)VALUES('Meeti
 
 INSERT INTO assigned(task_id,emp_id,role_id)VALUES(1,1,1);
 
-
+select * from accounts;
 select * from employees;
 select * from users;
 select * from team ;
 select * from role;
+-- Master table of role and employee    
 select * from team_member;
+
 select * from team_member where team_id=1;
 select * from project;
 select * from project_manager;
@@ -158,3 +148,26 @@ select * from task;
 select * from assigned;
 select * from timesheets;
 select * from payrollCycles;
+
+-- SELECT project.proj_id,project.proj_name,project.actual_startDate,project.actual_endDate,project.proj_desc,team_member.emp_id
+-- from project
+-- inner join team_member on  team_member.team_member_id =project.team_member_id
+-- inner join team_member on  team_member.emp_id = employees.emp_id
+-- where  project.proj_name = "Meeting Sheduling";
+-- 			
+-- SELECT movie_producers.id, movie_producers.title, reservations.number_of_seats, reservations.price, reservations.reservation_date, customers.id
+-- FROM reservations
+-- INNER JOIN customers ON customers.id = reservations.cust_id
+-- INNER JOIN seats ON seats.id = reservations.seat_id
+-- INNER JOIN screens ON screens.id = seats.screen_id
+-- INNER JOIN movie_producers ON movie_producers.id = screens.movie_id;
+
+-- CREATE TABLE reservations(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+--                             cust_id INT NOT NULL, 
+--                             CONSTRAINT fk_cust_id FOREIGN KEY(cust_id) REFERENCES customers(id) ON UPDATE CASCADE ON DELETE CASCADE,
+--                             reservation_date DATETIME,
+--                             screen_id INT NOT NULL, 
+--                             CONSTRAINT fk_screen_id1 FOREIGN KEY(screen_id) REFERENCES screens(id) ON UPDATE CASCADE ON DELETE CASCADE,
+--                             number_of_seats INT NOT NULL, 
+--                             price DOUBLE,     
+--                             total_amount DOUBLE AS (number_of_seats * price));
