0da22b83a94113c152b872d28a25237e37b3d8d0
AKSHAY TANPURE <116140237+ErAkshay2210@users.noreply.github.com>
2023-06-08 23:30:56 +0530

    Update ClientService.cs

--- WebServices/clientservice/Services/ClientService.cs
+++ WebServices/clientservice/Services/ClientService.cs
@@ -1,26 +1,26 @@
-// using PMS.Models;
-// using PMS.Repositories.Interfaces;
-// using PMS.Services.Interfaces;
-// namespace PMS.Services;
+using PMS.Models;
+using PMS.Repositories.Interfaces;
+using PMS.Services.Interfaces;
+namespace PMS.Services;
 
 
-// public class ClientServices : IClientServices
-// {
+public class ClientServices : IClientServices
+{
 
-//      private readonly IClientRepository _repo ;
+     private readonly IClientRepository _repo ;
    
-//     public ClientServices(IClientRepository repo){
-//         _repo=repo;
-//     }
-//     public List<Client> GetAll()=>_repo.GetAll();
+    public ClientServices(IClientRepository repo){
+        _repo=repo;
+    }
+    public List<Client> GetAll()=>_repo.GetAll();
 
-//     public Client Get(int id)=>_repo.Get(id);
+    public Client Get(int id)=>_repo.Get(id);
 
-//     public bool Insert(Client client)=>_repo.Insert(client);
+    public bool Insert(Client client)=>_repo.Insert(client);
 
-//     public bool Update(Client client)=>_repo.Update(client);
+    public bool Update(Client client)=>_repo.Update(client);
 
-//     public bool Delete(int id)=>_repo.Delete(id);
+    public bool Delete(int id)=>_repo.Delete(id);
 
 
-// }
+}
