74b830d93cd1d323f7c76a767776c2fb25377b41
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-06-09 18:40:10 +0530

    Grid Paging

--- Angular/FrontEnd/src/app/projectgrid/projectgrid.component.ts
+++ Angular/FrontEnd/src/app/projectgrid/projectgrid.component.ts
@@ -7,43 +7,42 @@ import { Project } from '../project';
   templateUrl: './projectgrid.component.html',
   styleUrls: ['./projectgrid.component.css']
 })
-export class ProjectgridComponent implements OnInit{
- projects:Project[]
-  startPosition:number=1;
-  endPosition:number=1;
-  selectedProjects:any[] | undefined;
- // expenseEntries:any[];
-  size:number=5;
-  constructor ( private svc : ProjectService){ 
-    this.projects = []; 
+export class ProjectgridComponent implements OnInit {
+  projects: Project[]
+  startPosition: number = 1;
+  endPosition: number = 1;
+  selectedProjects: any[] | undefined;
+  size: number = 5;
+  constructor(private svc: ProjectService) {
+    this.projects = [];
   }
   ngOnInit(): void {
     this.svc.getAllProjects().subscribe(
-      (response)=>{
-        this.projects=response;
+      (response) => {
+        this.projects = response;
         console.log(this.projects);
-          this.size=3;
-          this.startPosition=0;
-          this.endPosition=this.startPosition+this.size;
-          this.selectedProjects=this.projects.slice(this.startPosition,this.endPosition);
-        })
-        }
-      
-        onPrevious(){
-          this.startPosition=this.startPosition-this.size;
-          this.endPosition=this.startPosition+this.size;
-          this.selectedProjects=this.projects.slice(this.startPosition,this.endPosition);
-        }
-        onNext(){
-          this.startPosition=this.startPosition+this.size;
-          this.endPosition=this.startPosition+this.size;
-          this.selectedProjects=this.projects.slice(this.startPosition,this.endPosition);
-        }
-      
-
-
-
+        this.size = 2;
+        this.startPosition = 0;
+        this.endPosition = this.startPosition + this.size;
+        this.selectedProjects = this.projects.slice(this.startPosition, this.endPosition);
+      })
   }
+
+  onPrevious() {
+    this.startPosition = this.startPosition - this.size;
+    this.endPosition = this.startPosition + this.size;
+    this.selectedProjects = this.projects.slice(this.startPosition, this.endPosition);
+  }
+  onNext() {
+    this.startPosition = this.startPosition + this.size;
+    this.endPosition = this.startPosition + this.size;
+    this.selectedProjects = this.projects.slice(this.startPosition, this.endPosition);
+  }
+
+
+
+
+}
 
 
 
