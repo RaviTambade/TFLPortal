bf13faacf17a94edb5b4e8bdad632354cb32a241
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2023-06-09 16:06:31 +0530

    projectgrid

--- Angular/FrontEnd/src/app/projectgrid/projectgrid.component.ts
+++ Angular/FrontEnd/src/app/projectgrid/projectgrid.component.ts
@@ -8,20 +8,43 @@ import { Project } from '../project';
   styleUrls: ['./projectgrid.component.css']
 })
 export class ProjectgridComponent implements OnInit{
-
-
-  constructor ( private svc : ProjectService){ }
-
-  projects : Project[] | undefined; 
-
+ projects:Project[]
+  startPosition:number=1;
+  endPosition:number=1;
+  selectedProjects:any[] | undefined;
+ // expenseEntries:any[];
+  size:number=5;
+  constructor ( private svc : ProjectService){ 
+    this.projects = []; 
+  }
   ngOnInit(): void {
     this.svc.getAllProjects().subscribe(
       (response)=>{
         this.projects=response;
         console.log(this.projects);
-    })
+          this.size=3;
+          this.startPosition=0;
+          this.endPosition=this.startPosition+this.size;
+          this.selectedProjects=this.projects.slice(this.startPosition,this.endPosition);
+        })
+        }
+      
+        onPrevious(){
+          this.startPosition=this.startPosition-this.size;
+          this.endPosition=this.startPosition+this.size;
+          this.selectedProjects=this.projects.slice(this.startPosition,this.endPosition);
+        }
+        onNext(){
+          this.startPosition=this.startPosition+this.size;
+          this.endPosition=this.startPosition+this.size;
+          this.selectedProjects=this.projects.slice(this.startPosition,this.endPosition);
+        }
+      
+
+
+
   }
 
 
 
-}
+
