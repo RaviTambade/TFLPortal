e0cea282914b4f9270534a27ac0a357d0eb56c50
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-07-06 12:01:10 +0530

    Restructuring Project Files

--- ProjectManagementUI/src/app/credential.ts
+++
@@ -1,5 +0,0 @@
-export class Credential{
-    constructor(public email:string,
-                public password:string)
-                {}
-}
--- ProjectManagementUI/src/app/users.ts
+++
@@ -1,7 +0,0 @@
-export class Users{
-    constructor(
-                public contactNumber:string,
-                public email:string,
-                public password:string)
-                {}
-}
