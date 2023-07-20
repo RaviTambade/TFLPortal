b90c106eb476b29f99a6c108893de8bbfb799cc7
AKSHAY TANPURE <116140237+ErAkshay2210@users.noreply.github.com>
2023-07-20 10:54:03 +0530

    Update createdb.sql

--- Databases/createdb.sql
+++ Databases/createdb.sql
@@ -10,7 +10,6 @@ CREATE TABLE accounts(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                       ifsccode VARCHAR(50) ,
                       registerdate DATETIME ,
                       balance DOUBLE);
-  
                   
 CREATE TABLE employees(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
                       firstname VARCHAR (50),
@@ -23,16 +22,13 @@ CREATE TABLE employees(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                       CONSTRAINT fk_account_no1 FOREIGN KEY(accountnumber) REFERENCES accounts(accountnumber) ON UPDATE CASCADE ON DELETE CASCADE,
                       userid INT NOT NULL); 
 
-
 CREATE TABLE roles(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                    rolename varchar (50));
-  
                 
 CREATE TABLE userroles(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 		     	       userid INT NOT NULL, 
                        roleid INT NOT NULL,
                        CONSTRAINT fk_role_id2 FOREIGN KEY(roleid) REFERENCES roles(id) ON UPDATE CASCADE ON DELETE CASCADE);
-
 
 CREATE TABLE projects(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                      title VARCHAR(100),
@@ -40,14 +36,12 @@ CREATE TABLE projects(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                      enddate DATETIME NOT NULL,
                      description VARCHAR(255),
                      status enum('Pending','In-Progress','Completed','Error'));
-
-                     
+                    
 CREATE TABLE projectmembers(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                             projectid INT NOT NULL, 
                             CONSTRAINT fk_proid FOREIGN KEY (projectid) REFERENCES projects(id)ON UPDATE CASCADE  ON DELETE CASCADE,
                             empid INT NOT NULL, 
                             CONSTRAINT fk_emp_Id4 FOREIGN KEY(empid) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE );
-
 
 CREATE TABLE clients(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                     fullname VARCHAR(50),
@@ -56,15 +50,13 @@ CREATE TABLE clients(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                     accountnumber VARCHAR(25) NOT NULL UNIQUE ,
                     CONSTRAINT fk_account_no2 FOREIGN KEY(accountnumber) REFERENCES accounts(accountnumber) ON UPDATE CASCADE ON DELETE CASCADE
                     );
-  
-                 
+               
 CREATE TABLE onproject(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                       projectid INT NOT NULL, 
                       CONSTRAINT fk_projid FOREIGN KEY (projectid) REFERENCES projects(id)ON UPDATE CASCADE  ON DELETE CASCADE,
                       clientid INT NOT NULL, 
                       CONSTRAINT fk_clientid FOREIGN KEY (clientid) REFERENCES clients(id)ON UPDATE CASCADE  ON DELETE CASCADE);
                   
-   
 CREATE TABLE tasks(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                   title VARCHAR(50),
                   projectid INT NOT NULL, 
@@ -73,8 +65,7 @@ CREATE TABLE tasks(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                   date DATETIME,
                   fromtime TIME,
                   totime TIME);
-  
-               
+              
 CREATE TABLE assigned(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                      taskid INT NOT NULL, 
                      CONSTRAINT fk_taskid FOREIGN KEY (taskid) REFERENCES tasks(id)ON UPDATE CASCADE  ON DELETE CASCADE,
@@ -82,8 +73,7 @@ CREATE TABLE assigned(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                      CONSTRAINT fk_empidd FOREIGN KEY (empid) REFERENCES employees(id)ON UPDATE CASCADE  ON DELETE CASCADE,
                      roleid INT NOT NULL, 
                      CONSTRAINT fk_roleidd FOREIGN KEY (roleid) REFERENCES roles(id)ON UPDATE CASCADE  ON DELETE CASCADE);
-
-                    
+               
 CREATE TABLE payrollCycles(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                            payrollcycleyear DATETIME,
                            payrollcyclenumber INT NOT NULL,
@@ -92,8 +82,7 @@ CREATE TABLE payrollCycles(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                            accountnumber VARCHAR(25) NOT NULL UNIQUE ,
                            CONSTRAINT fk_account_no3 FOREIGN KEY(accountnumber) REFERENCES accounts(accountnumber) ON UPDATE CASCADE ON DELETE CASCADE,
                            depositdate DATETIME);
-  
-                            
+                        
  CREATE TABLE timesheets (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                          date DATETIME,
                          fromtime TIME,
@@ -102,7 +91,6 @@ CREATE TABLE payrollCycles(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                          empid INT NOT NULL, CONSTRAINT fk_emp_Id5 FOREIGN KEY(empid) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE,
                          projectid INT NOT NULL, CONSTRAINT fk_project_Id2 FOREIGN KEY(projectid) REFERENCES projects(id) ON UPDATE CASCADE ON DELETE CASCADE,
                          taskid INT NOT NULL, CONSTRAINT fk_task_Id2 FOREIGN KEY(taskid) REFERENCES tasks(id) ON UPDATE CASCADE ON DELETE CASCADE);
-
 
  CREATE TABLE timerecords (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 						  date DATE,
