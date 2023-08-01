438c04e3f11b78cfba6353ceb4ced9d5d173f5ef
AKSHAY TANPURE <116140237+ErAkshay2210@users.noreply.github.com>
2023-08-01 10:50:18 +0530

    Update FundsTransferController.cs

--- API/PaymentGateWay/Controllers/FundsTransferController.cs
+++ API/PaymentGateWay/Controllers/FundsTransferController.cs
@@ -5,19 +5,17 @@ using API.Services.Interfaces;
 using PMS.Helpers;
 
 namespace PaymentGateWayService.Controllers;
-
 //[Authorize]
 [ApiController]
 [Route("/api/[controller]")]
 public class FundTransferController: ControllerBase
 {
-
     private readonly IPaymentGatewayService _svc;
     public FundTransferController(IPaymentGatewayService svc)
     {
         _svc = svc;
     }
-
+    
     //httpPost : http://localhost:5041/api/fundtransfer
     [HttpPost]
     public async Task<int> PaymentGateWay([FromBody] PaymentGateWay info)
