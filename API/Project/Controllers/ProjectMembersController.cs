5dd7c88ae30a0b888d5e7ffb02786f6bbb96bfb9
AKSHAY TANPURE <116140237+ErAkshay2210@users.noreply.github.com>
2023-08-02 23:50:00 +0530

    Update ProjectMembersController.cs

--- API/Project/Controllers/ProjectMembersController.cs
+++ API/Project/Controllers/ProjectMembersController.cs
@@ -1,76 +1,76 @@
-using PMS.Models;
-using PMS.Services.Interfaces;
-using Microsoft.AspNetCore.Mvc;
+//using PMS.Models;
+//using PMS.Services.Interfaces;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace TeamService.Controllers;
+//namespace TeamService.Controllers;
 
-[ApiController]
-[Route("/api/[controller]")]
-public class ProjectMembersController : ControllerBase
-{
-    private readonly IProjectMemberService _service;
-    public ProjectMembersController(IProjectMemberService service)
-    {
-        _service = service;
-    }
+//[ApiController]
+//[Route("/api/[controller]")]
+//public class ProjectMembersController : ControllerBase
+//?{
+  //  private readonly IProjectMemberService _service;
+//    public ProjectMembersController(IProjectMemberService service)
+//    {
+//        _service = service;
+//    }
+//   
+//    //httpGet :  http://localhost:5294/api/ProjectMembers/ProjectMembers
+//    [HttpGet]
+//    [Route("ProjectMembers")]
+ //   public async Task<IEnumerable<ProjectMember>> GetAllTeamMembers()
+//    {
+ //       IEnumerable<ProjectMember> projectmembers =await _service.GetAll();
+ //       return projectmembers;
+//    }
+
+ //   //httpGet : http://localhost:5294/api/ProjectMembers/1
+//    [HttpGet ]
+//    [Route ("{id}")]
+//    public async Task<ProjectMember> GetById(int id)
+//    {
+//        ProjectMember projectmember =await _service.GetById(id);
+//        return projectmember;
+//    }
    
-    //httpGet :  http://localhost:5294/api/ProjectMembers/ProjectMembers
-    [HttpGet]
-    [Route("ProjectMembers")]
-    public async Task<IEnumerable<ProjectMember>> GetAllTeamMembers()
-    {
-        IEnumerable<ProjectMember> projectmembers =await _service.GetAll();
-        return projectmembers;
-    }
+//     //httpGet : http://localhost:5294/api/ProjectMembers/project/1
+//     [HttpGet]
+//    [Route ("project/{projectId}")]
+//    public async Task<IEnumerable<ProjectMemberInfo>> Get(int projectId)
+ //   {
+//        IEnumerable<ProjectMemberInfo> projectmembers =await _service.Get(projectId);
+//        return projectmembers;
+//    }
 
-    //httpGet : http://localhost:5294/api/ProjectMembers/1
-    [HttpGet ]
-    [Route ("{id}")]
-    public async Task<ProjectMember> GetById(int id)
-    {
-        ProjectMember projectmember =await _service.GetById(id);
-        return projectmember;
-    }
-   
-     //httpGet : http://localhost:5294/api/ProjectMembers/project/1
-     [HttpGet]
-    [Route ("project/{projectId}")]
-    public async Task<IEnumerable<ProjectMemberInfo>> Get(int projectId)
-    {
-        IEnumerable<ProjectMemberInfo> projectmembers =await _service.Get(projectId);
-        return projectmembers;
-    }
-
-    //httpPost:  http://localhost:5294/api/ProjectMembers/ProjectMember
-    [HttpPost ]
-    [Route ("ProjectMember")]
-    public async Task<bool> Insert(ProjectMember projectMember)
-    {
-        bool status =await _service.Insert(projectMember);
-        return status;
-    }
+//    //httpPost:  http://localhost:5294/api/ProjectMembers/ProjectMember
+//    [HttpPost ]
+//    [Route ("ProjectMember")]
+//    public async Task<bool> Insert(ProjectMember projectMember)
+//    {
+ //        bool status =await _service.Insert(projectMember);
+//        return status;
+//    }
     
-    //httpPut : http://localhost:5294/api/ProjectMembers/17
-    [HttpPut ]
-    [Route("{id}")]
-    public async Task<bool> Update(ProjectMember projectMember)
-    {
-        bool status =await _service.Update(projectMember);
-        return status;
-    }
+//    //httpPut : http://localhost:5294/api/ProjectMembers/17
+//    [HttpPut ]
+//    [Route("{id}")]
+//    public async Task<bool> Update(ProjectMember projectMember)
+//    {
+//        bool status =await _service.Update(projectMember);
+//        return status;
+//    }
 
-    //httpDelete : http://localhost:5294/api/ProjectMembers/17
-    [HttpDelete ]
-    [Route("{id}")]
-    public async Task<bool> Delete(int id)
-    {
-        bool status =await _service.Delete(id);
-        return status;
-    }
-
+//    //httpDelete : http://localhost:5294/api/ProjectMembers/17
+//    [HttpDelete ]
+//    [Route("{id}")]
+ //    public async Task<bool> Delete(int id)
+ //   {
+ //       bool status =await _service.Delete(id);
+//        return status;
+//    }
+//
 
    
-}
+//}
 
 
 
