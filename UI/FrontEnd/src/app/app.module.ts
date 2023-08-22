59d9ceb24c72b6d530da0a0ea3bc8e185c2a952a
unknown <shubhamteli203@gmail.com>
2023-08-22 13:00:49 +0530

    Code Review

--- UI/FrontEnd/src/app/app.module.ts
+++ UI/FrontEnd/src/app/app.module.ts
@@ -1,40 +1,26 @@
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectListComponent } from './project-list/project-list.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MembersComponent } from './members/members.component';
-import { ProjectgridComponent } from './projectgrid/projectgrid.component';
-import { PaginationComponent } from './pagination/pagination.component';
-import { SortedListComponent } from './sorted-list/sorted-list.component';
-import { CheckBoxComponent } from './check-box/check-box.component';
-import { RolesComponent } from './roles/roles.component';
-import { RouterContainerComponent } from './managerview/router-container/router-container.component';
-import { RouterModule } from '@angular/router';
 import { ManagerviewModule } from './managerview/managerview.module';
 import { ProjectstatusModule } from './managerview/chart/projectstatus/projectstatus.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectListComponent,
-    MembersComponent,
-    ProjectgridComponent,
-    PaginationComponent,
-    SortedListComponent,
-    CheckBoxComponent,
-    RolesComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
     ManagerviewModule,
-    ProjectstatusModule
+    ProjectstatusModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
