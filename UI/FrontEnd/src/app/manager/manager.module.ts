b2f27970eb81d496ead12507dc2b9ab94d694f3c
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2023-09-04 23:34:55 +0530

    project component

--- UI/FrontEnd/src/app/manager/manager.module.ts
+++ UI/FrontEnd/src/app/manager/manager.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProjectListComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ UI/FrontEnd/src/app/manager/project-list/project-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.css']
+})
+export class ProjectListComponent {
+
+}
