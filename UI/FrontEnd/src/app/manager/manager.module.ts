ea2d49d42863f133abf450c63335123626634625
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2023-09-06 00:14:54 +0530

    project-register component is added

--- UI/FrontEnd/src/app/manager/manager.module.ts
+++ UI/FrontEnd/src/app/manager/manager.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectRegisterComponent } from './project-register/project-register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +15,13 @@ import { ProjectRegisterComponent } from './project-register/project-register.co
     ProjectRegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule,
+
   ]
 })
 export class ManagerModule { }
--- UI/FrontEnd/src/app/manager/project-register/project-register.component.ts
+++ UI/FrontEnd/src/app/manager/project-register/project-register.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from 'src/app/models/employee';
+import { Project } from 'src/app/models/project';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-project-register',
   templateUrl: './project-register.component.html',
   styleUrls: ['./project-register.component.css']
 })
-export class ProjectRegisterComponent {
+export class ProjectRegisterComponent implements OnInit {
+
+  project: Project | any;
+  Id: number | any;
+  status : boolean |undefined;
+
+  constructor(private svc: EmployeeService, private router: Router, private route: ActivatedRoute) {
+    this.project = {
+      id: 0,
+      title: '',
+      startDate: '',
+      endDate: '',
+      description: '',
+    };
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.Id = params.get('id')
+    })
+
+  }
+
+  addProject(form:any): void {
+    console.log(form);
+    console.log(this.project);
+    this.svc.addProject(this.project).subscribe((response) => {
+      this.status = response;
+      console.log(response);
+      if (response) {
+        alert("Employee added successfully")
+        this.router.navigate(['/project-list']);
+        window.location.reload();
+      }
+      else {
+        alert("Check the form again ....")
+      }
+    })
+  };
 
 }
+ 
