2cccf96483f5b67dbd583be1d598521911330ec2
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-09-15 09:19:04 +0530

    adsf

--- UI/LatestNGApp/PMSUIApp/src/app/Services/project.service.ts
+++ UI/LatestNGApp/PMSUIApp/src/app/Services/project.service.ts
@@ -5,15 +5,15 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class ProjectService {
-   projects: {id:number, employeeId:number; title: string; startDate: Date; status: string;description:string;teamManager:string }[] = [
-    {id:1,employeeId:1, title: "PMSAPP", startDate: new Date("2023-02-02"), status: "In-Progress",description: "Description for Project 1", teamManager: "Manager 1" },
-    {id:2,employeeId:1, title: "EKrushi", startDate: new Date("2023-02-02"), status: "Pending",description: "Description for Project 2", teamManager: "Manager 2"  },
-    {id:3,employeeId:1, title: "HMAPP", startDate: new Date("2023-02-02"), status: "In-Progress",description: "Description for Project 2", teamManager: "Manager 2" },
-    {id:4,employeeId:1, title: "EAgro", startDate: new Date("2023-02-02"), status: "Pending" ,description: "Description for Project 2", teamManager: "Manager 2"},
-    {id:5,employeeId:1, title: "Inventory", startDate: new Date("2023-02-02"), status: "Completed",description: "Description for Project 2", teamManager: "Manager 2" },
-    {id:6,employeeId:1, title: "OMTBAPP", startDate: new Date("2023-02-02"), status: "Completed" ,description: "Description for Project 2", teamManager: "Manager 2"},
-    {id:7,employeeId:1, title: "OCBAPP", startDate: new Date("2023-02-02"), status: "Canceled" ,description: "Description for Project 2", teamManager: "Manager 2"},
-    {id:8,employeeId:1, title: "EKrushi", startDate: new Date("2023-02-02"), status: "Canceled",description: "Description for Project 2", teamManager: "Manager 2" },
+   projects: {id:number, employeeId:number; title: string; startDate: Date; status: string;completion:string;description:string;teamManager:string }[] = [
+    {id:1,employeeId:1, title: "PMSAPP", startDate: new Date("2023-02-02"), status: "In-Progress",completion:"50%",description: "Description for Project 1", teamManager: "Manager 1" },
+    {id:2,employeeId:1, title: "EKrushi", startDate: new Date("2023-02-02"), status: "Pending",completion:"0%",description: "Description for Project 2", teamManager: "Manager 2"  },
+    {id:3,employeeId:1, title: "HMAPP", startDate: new Date("2023-02-02"), status: "In-Progress",completion:"30%",description: "Description for Project 2", teamManager: "Manager 2" },
+    {id:4,employeeId:1, title: "EAgro", startDate: new Date("2023-02-02"), status: "Pending" ,completion:"0%",description: "Description for Project 2", teamManager: "Manager 2"},
+    {id:5,employeeId:1, title: "Inventory", startDate: new Date("2023-02-02"), status: "Completed",completion:"100%",description: "Description for Project 2", teamManager: "Manager 2" },
+    {id:6,employeeId:1, title: "OMTBAPP", startDate: new Date("2023-02-02"), status: "Completed" ,completion:"100%",description: "Description for Project 2", teamManager: "Manager 2"},
+    {id:7,employeeId:1, title: "OCBAPP", startDate: new Date("2023-02-02"), status: "Canceled" ,completion:"0%",description: "Description for Project 2", teamManager: "Manager 2"},
+    {id:8,employeeId:1, title: "EKrushi", startDate: new Date("2023-02-02"), status: "Canceled",completion:"0%",description: "Description for Project 2", teamManager: "Manager 2" },
   ];
   projectTeamMembers:{projectId:number;teammembers:string[]}[]=[
     {projectId:1,teammembers:["Rushikesh Chikane" ,"Abhay Navale","Akshay Tanpure"]},
@@ -40,6 +40,8 @@ export class ProjectService {
       id: project.id,
       title: project.title,
       status: project.status,
+      completion:project.completion,
+      teamManager:project.teamManager,
       startDate: project.startDate
     }));
     return of(summaryData);
