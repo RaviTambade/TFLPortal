6e62c442c8ed15cdf1b5b24e85ecb7ce5fefe80e
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-09-24 22:13:23 +0530

    UI

--- UI/LatestNGApp/PMSUIApp/src/app/Services/project.service.ts
+++ UI/LatestNGApp/PMSUIApp/src/app/Services/project.service.ts
@@ -12,59 +12,59 @@ import { Unassignedtask } from '../Models/unassignedtask';
   providedIn: 'root'
 })
 export class ProjectService {
-  
 
-  constructor(private httpClient:HttpClient) { }
-  private selectedProjectIdSubject=new BehaviorSubject<any>(null);
-  selectedProjectId$=this.selectedProjectIdSubject.asObservable();
-  
-  setSelectedProjectId(id:number|null){
+
+  constructor(private httpClient: HttpClient) { }
+  private selectedProjectIdSubject = new BehaviorSubject<any>(null);
+  selectedProjectId$ = this.selectedProjectIdSubject.asObservable();
+
+  setSelectedProjectId(id: number | null) {
     this.selectedProjectIdSubject.next(id)
   }
 
-  getProjectsList(teamMemberId:number):Observable<Projectlist[]>{
-    let url="http://localhost:5248/api/projects/list/" +teamMemberId
+  getProjectsList(teamMemberId: number): Observable<Projectlist[]> {
+    let url = "http://localhost:5248/api/projects/list/" + teamMemberId
     return this.httpClient.get<Projectlist[]>(url)
   }
 
-  getProjectDetails(projectId:number):Observable<Project>{
-    let url="http://localhost:5248/api/projects/" +projectId
+  getProjectDetails(projectId: number): Observable<Project> {
+    let url = "http://localhost:5248/api/projects/" + projectId
     return this.httpClient.get<Project>(url)
   }
 
-  getProjectMembers(projectId:number):Observable<number[]>{
-    let url="http://localhost:5248/api/projects/teammembers/" +projectId
+  getProjectMembers(projectId: number): Observable<number[]> {
+    let url = "http://localhost:5248/api/projects/teammembers/" + projectId
     return this.httpClient.get<number[]>(url)
   }
 
-  getTasksOfProject(projectId:number,timePeriod:string):Observable<Projecttask[]>{
-    let url="http://localhost:5248/api/projects/tasks/" +projectId + "/" + timePeriod 
+  getTasksOfProject(projectId: number, timePeriod: string): Observable<Projecttask[]> {
+    let url = "http://localhost:5248/api/projects/tasks/" + projectId + "/" + timePeriod
     return this.httpClient.get<Projecttask[]>(url)
   }
-  getProjectNames(employeeId:number):Observable<Projectname[]>{
-    let url="http://localhost:5248/api/projects/employee/" +employeeId
+  getProjectNames(employeeId: number): Observable<Projectname[]> {
+    let url = "http://localhost:5248/api/projects/employee/" + employeeId
     return this.httpClient.get<Projectname[]>(url)
   }
 
-  getManagerProjects(managerId:number):Observable<Projectlist[]>{
-    let url="http://localhost:5248/api/projects/manager/" + managerId
+  getManagerProjects(managerId: number): Observable<Projectlist[]> {
+    let url = "http://localhost:5248/api/projects/manager/" + managerId
     return this.httpClient.get<Projectlist[]>(url)
   }
 
-  addProject(project:Addproject):Observable<boolean>{
-    let url="http://localhost:5248/api/projects/addproject"
-    return this.httpClient.post<boolean>(url,project) 
+  addProject(project: Addproject): Observable<boolean> {
+    let url = "http://localhost:5248/api/projects/addproject"
+    return this.httpClient.post<boolean>(url, project)
   }
-  updateProject(project:Project):Observable<boolean>{
-    let url="http://localhost:5248/api/projects"
-    return this.httpClient.put<boolean>(url,project)
+  updateProject(project: Project): Observable<boolean> {
+    let url = "http://localhost:5248/api/projects"
+    return this.httpClient.put<boolean>(url, project)
   }
-  deleteProject(projectId:number):Observable<boolean>{
-    let url="http://localhost:5248/api/projects/" +projectId
+  deleteProject(projectId: number): Observable<boolean> {
+    let url = "http://localhost:5248/api/projects/" + projectId
     return this.httpClient.delete<boolean>(url)
   }
-  unAssignedTask(projectId:number,timePeriod:string):Observable<Unassignedtask[]>{
-    let url="http://localhost:5248/api/projects/unassignedtask/" + projectId +  "/" + timePeriod
+  unAssignedTask(projectId: number, timePeriod: string): Observable<Unassignedtask[]> {
+    let url = "http://localhost:5248/api/projects/unassignedtask/" + projectId + "/" + timePeriod
     return this.httpClient.get<Unassignedtask[]>(url)
   }
 }
