4ad4f6bed001dbed6bc7d7b6ff7db25306d4f445
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-09-29 21:59:47 +0530

    UI

--- UI/LatestNGApp/PMSUIApp/src/app/Services/employee.service.ts
+++ UI/LatestNGApp/PMSUIApp/src/app/Services/employee.service.ts
@@ -7,24 +7,24 @@ import { Employeeinfo } from '../Models/employeeinfo';
   providedIn: 'root',
 })
 export class EmployeeService {
-  constructor(private httpClient :HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
 
-  getEmployeeId(userId:number):Observable<number>{
-    let url= "http://localhost:5230/api/Employees/employeeid/" +userId
+  getEmployeeId(userId: number): Observable<number> {
+    let url = "http://localhost:5230/api/Employees/employeeid/" + userId
     return this.httpClient.get<number>(url)
   }
-  getUserId(employeeId:string):Observable<string[]>{
-    let url= "http://localhost:5230/api/Employees/userid/" + employeeId
+  getUserId(employeeId: string): Observable<string[]> {
+    let url = "http://localhost:5230/api/Employees/userid/" + employeeId
     return this.httpClient.get<string[]>(url)
   }
-  getEmployeeInfo(employeeId:number):Observable<Employeeinfo>{
-    let url="http://localhost:5230/api/Employees/employeeinfo/" +employeeId
+  getEmployeeInfo(employeeId: number): Observable<Employeeinfo> {
+    let url = "http://localhost:5230/api/Employees/employeeinfo/" + employeeId
     return this.httpClient.get<Employeeinfo>(url)
   }
 
-  getUserIdByManagerId(managerId:number):Observable<number[]>{
-    let url="http://localhost:5230/api/Employees/useridbymanager/" +managerId
+  getUserIdByManagerId(managerId: number): Observable<number[]> {
+    let url = "http://localhost:5230/api/Employees/useridbymanager/" + managerId
     return this.httpClient.get<number[]>(url)
   }
 }
