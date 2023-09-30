416ac583faeb0ac3ac4162d21323cabaeeb1b1f8
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-09-30 16:46:43 +0530

    UI

--- UI/LatestNGApp/PMSUIApp/src/app/hrmanager/hrmanager.module.ts
+++ UI/LatestNGApp/PMSUIApp/src/app/hrmanager/hrmanager.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Routes } from '@angular/router';
+import { EmployeeslistComponent } from './employeeslist/employeeslist.component';
 
 
 export const hrmanagerRoutes:Routes=[
   {path:'dashboard',component:DashboardComponent},
+  {path:'employeeslist',component:EmployeeslistComponent},
 ]
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    EmployeeslistComponent,
    
   ],
   imports: [
