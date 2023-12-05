66ff4ac865a008e1ef65991ff24fcba4b840fa8f
PragatiBangar <bangarpragati11@gmail.com>
2023-12-05 15:07:32 +0530

    api added

--- WebSolution/Intranet/Responses/ProjectAllocationResponse.cs
+++ WebSolution/Intranet/Responses/ProjectAllocationResponse.cs
@@ -2,8 +2,10 @@ using Transflower.TFLPortal.TFLSAL.DTO;
 
 public class ProjectAllocationResponse
 {
+    public  int Id{ get; set; }
     public string? FullName { get; set; }
     public int EmployeeId {get; set;}
     public string? Membership { get; set; }
     public DateTime AssignDate { get; set; }
+    public  int ProjectId{ get; set; }
 }
