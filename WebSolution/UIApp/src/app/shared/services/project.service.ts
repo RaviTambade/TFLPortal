573784677fa79ba7ca66d8242f10e2f5ee06c141
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-12-07 18:27:07 +0530

    restructuring

--- WebSolution/UIApp/src/app/shared/services/project.service.ts
+++ WebSolution/UIApp/src/app/shared/services/project.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from 'src/app/projects/Models/project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+ 
+   
+  private serviceurl :string=environment.apiUrl;
+
+  getAllProject():Observable<Project[]>{
+    let url=this.serviceurl+"/projectmgmt/projects";
+    return this.httpClient.get<Project[]>(url);
+  }
+  
+  getAllProjects():Observable<Project[]>{
+    let url=this.serviceurl+"/projectmgmt/projects";
+    return this.httpClient.get<Project[]>(url);
+  
+  }
+
+  getProjectDetails(projectId:number):Observable<any>{
+    let url=this.serviceurl+"/projectmgmt/projects"+"/"+projectId;
+    console.log(url);
+    return this.httpClient.get<any>(url);
+  }
+
+  getAllEmployees(projectId:number):Observable<any[]>{
+    let url=this.serviceurl+"/projectmgmt/projectallocation/employees/"+projectId;
+    return this.httpClient.get<any[]>(url);
+  }
+  
 }
--- WebSolution/UIApp/src/app/shared/services/workmgmt.service.ts
+++ WebSolution/UIApp/src/app/shared/services/workmgmt.service.ts
@@ -1,9 +1,77 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Activity } from 'src/app/activity/Models/Activity';
+import { Employee } from 'src/app/activity/Models/Employee';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WorkmgmtService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  private serviceurl: string = environment.apiUrl;
+
+  // http://localhost:5263/api/workmgmt/activities/1
+
+  addActivity(addactivity: Activity): Observable<boolean> {
+    let url = this.serviceurl + '/workmgmt/activities/insert';
+    console.log('service called');
+    return this.httpClient.post<boolean>(url, addactivity);
+  }
+
+  fetchActivitiesByProject(projectId: number): Observable<Activity[]> {
+    let url = this.serviceurl + '/workmgmt/activities/' + projectId;
+    console.log(url);
+    return this.httpClient.get<Activity[]>(url);
+  }
+
+  fetchActivitiesByProjectAndAssigner(
+    projectId: number,
+    assignedTo: number
+  ): Observable<Activity[]> {
+    let url =
+      this.serviceurl +
+      '/workmgmt/activities/activity' +
+      '/' +
+      projectId +
+      '/' +
+      assignedTo;
+    console.log(url);
+    return this.httpClient.get<Activity[]>(url);
+  }
+
+  getEmployeeDetails(employeeId: number): Observable<Employee> {
+    let url = this.serviceurl + '/hr/employees/employee' + '/' + employeeId;
+    console.log(url);
+    return this.httpClient.get<Employee>(url);
+  }
+
+  // http://localhost:5263/api/workmgmt/activities/activity/todo/4/15
+
+  getAllNotStartedActivities(
+    projectId: number,
+    employeeId: number
+  ): Observable<Activity[]> {
+    let url =
+      this.serviceurl +
+      '/workmgmt/activities/activity/todo/' +
+      projectId +
+      '/' +
+      employeeId;
+    return this.httpClient.get<Activity[]>(url);
+  }
+
+  // http://localhost:5263/api/workmgmt/activities/Update/1/4/15
+
+  updateActivity(status: string, activityId: number): Observable<boolean> {
+    let url =
+      this.serviceurl +
+      '/workmgmt/activities/Update/' +
+      status +
+      '/' +
+      activityId;
+    return this.httpClient.put<boolean>(url, status);
+  }
 }
