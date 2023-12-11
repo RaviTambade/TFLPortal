d97b4a7354afce0725e24fd1f857a75c59b426d1
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-12-11 22:06:28 +0530

    new changes

--- WebSolution/UIApp/src/app/bi/piechart/piechart.component.ts
+++ WebSolution/UIApp/src/app/bi/piechart/piechart.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-piechart',
   templateUrl: './piechart.component.html',
   styleUrls: ['./piechart.component.css']
 })
-export class PiechartComponent {
+export class PiechartComponent implements OnInit{
+  title = 'ChartApp';
 
+  chart: any = [];
+ 
+  
+
+  createChart(){
+  
+    this.chart = new Chart("MyChart", {
+      type:'pie', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+									 '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          },
+          {
+            label: "margin",
+            data: ['120', '125', '127', '70', '0',
+									 '0.00', '129', '200'],
+            backgroundColor: 'green'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+  }
+
+  ngOnInit() {
+    this.createChart();
+  }
 }
