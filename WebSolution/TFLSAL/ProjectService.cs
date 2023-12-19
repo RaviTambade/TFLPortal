b0fd515f12b52c439ca2b0878c3a5d7865731b89
sahilmankar <sahilmankar311@gmail.com>
2023-12-19 12:48:33 +0530

    db updated

--- WebSolution/TFLSAL/ProjectService.cs
+++ WebSolution/TFLSAL/ProjectService.cs
@@ -72,8 +72,10 @@ public class ProjectService : IProjectService
         connection.ConnectionString = _connectionString;
         try
         {
+            // ptojects.id =9 because 10 number is other 
             string query =
-                "select * from projects inner join projectallocations  on projects.id=projectallocations.projectid where projectallocations.employeeid=@employeeid";
+                @"SELECT * FROM projects WHERE projects.id=9 OR
+                id IN( SELECT DISTINCT projectid FROM projectallocations WHERE projectallocations.employeeid=@employeeid )"; 
             MySqlCommand command = new MySqlCommand(query, connection);
             command.Parameters.AddWithValue("@employeeid", employeeid);
             await connection.OpenAsync();
