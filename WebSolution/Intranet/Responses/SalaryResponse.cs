27eaaa6ade83311dd2bd53711d38f32a1bf483b8
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2024-01-12 12:55:24 +0530

    salaryStructure added

--- WebSolution/Intranet/Responses/SalaryResponse.cs
+++ WebSolution/Intranet/Responses/SalaryResponse.cs
@@ -1,21 +1,17 @@
 
+using Transflower.TFLPortal.TFLOBL.Entities;
+
 namespace Transflower.TFLPortal.Intranet.Responses;
 
-public class SalaryResponse
+public class SalaryResponse:MonthSalary
 {
     public int EmployeeId { get; set; }
-    public double BasicSalary { get; set; }
-    public double HRA { get; set; }
-    public double DA { get; set; }
-    public double LTA { get; set; }
-    public double VariablePay { get; set; }
-    public double Deduction { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? ContactNumber { get; set; }
     public DateOnly BirthDate { get; set; }
     public string? AccountNumber { get; set; }
     public string? IFSC { get; set; }
-
+    
     
 }
--- WebSolution/UIApp/src/app/hrmanager/components/payroll/monthlysalarystructure/monthlysalarystructure.component.ts
+++ WebSolution/UIApp/src/app/hrmanager/components/payroll/monthlysalarystructure/monthlysalarystructure.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
+import { MonthSalary } from 'src/app/resource-management/Models/MonthSalary';
 import { HrService } from 'src/app/shared/services/hr.service';
+import { PayrollService } from 'src/app/shared/services/payroll.service';
 
 @Component({
   selector: 'app-monthlysalarystructure',
@@ -9,13 +11,14 @@ import { HrService } from 'src/app/shared/services/hr.service';
 export class MonthlysalarystructureComponent {
 
   @Input() employeeId:number|undefined;
-  salaryStructure:any;
+  salaryStructure:MonthSalary|undefined;
 
-  constructor(private hrService:HrService){}
-  
+  constructor(private payRollService:PayrollService){}
+  month:number=1;
+  year:number=2024;
   ngOnChanges(changes:SimpleChanges): void {
     console.log(changes["employeeId"].currentValue);
-    this.hrService.getSalaryStructure(changes["employeeId"].currentValue).subscribe((res)=>{
+    this.payRollService.getEmployeeSalary(changes["employeeId"].currentValue,this.month,this.year).subscribe((res)=>{
       this.salaryStructure=res;
       console.log(res);
       console.log("🚀 ~ this.hrService.getSalaryStructure ~ res:", res);
