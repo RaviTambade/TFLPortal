2470ab56c7ec372111c8e9e8c73605a0e19a403f
PragatiBangar <bangarpragati11@gmail.com>
2024-02-02 13:15:33 +0530

    changes

--- Solution/WebSolution/Monolithic/Models/Employee.cs
+++ Solution/WebSolution/Monolithic/Models/Employee.cs
@@ -3,7 +3,6 @@ namespace TFLPortal.Models;
 public class Employee
 {
     public int Id { get; set; }
-    public int UserId { get; set; } 
     public DateTime HiredOn { get; set; }
     public int ReportingId { get; set; } 
 }
--- Solution/WebSolution/Monolithic/Services/Interfaces/IHRService.cs
+++ Solution/WebSolution/Monolithic/Services/Interfaces/IHRService.cs
@@ -5,7 +5,6 @@ public interface IHRService {
 
     Task<Employee> GetEmployeeById(int employeeId); 
     Task<List<Employee>> GetEmployees(string employeeIds); 
-    Task<Employee> GetEmployeeByUserId(int userId); 
     Task<List<Employee>> GetUnPaidSalaries(int month,int year);
 
  }
