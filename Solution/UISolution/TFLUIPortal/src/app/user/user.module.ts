55347d517a5377df0de7a7d4b28619669c8dd0c3
Ravi Tambade <ravi.tambade@transflower.in>
2024-02-05 16:49:52 +0530

    changes for restructuring

--- Solution/UISolution/TFLUIPortal/src/app/user/user.module.ts
+++ Solution/UISolution/TFLUIPortal/src/app/user/user.module.ts
@@ -4,6 +4,8 @@ import { UserProfileComponent } from './components/user-profile/user-profile.com
 import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes } from '@angular/router';
+import { NewUserComponent } from './components/new-user/new-user.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 export const userRoutes: Routes = [
   {path: 'profile', component: UserProfileComponent}
@@ -13,13 +15,17 @@ export const userRoutes: Routes = [
 @NgModule({
   declarations: [
     UpdateProfileComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    NewUserComponent,
+    UsersListComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  exports:[UserProfileComponent]
+  exports:[UserProfileComponent,
+        NewUserComponent        
+  ]
 })
 export class UserModule { }
