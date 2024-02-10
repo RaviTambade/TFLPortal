9455c5e8544978dff076e90a7da4a6e143ff2168
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2024-02-10 22:03:21 +0530

    users name

--- Solution/UISolution/TFLUIPortal/src/app/ProjectManager/components/sprintactivities/sprintactivities.ts
+++ Solution/UISolution/TFLUIPortal/src/app/ProjectManager/components/sprintactivities/sprintactivities.ts
@@ -2,20 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectService } from 'src/app/shared/services/ProjectMgmt/project.service';
 import { TaskModel } from 'src/app/shared/Models/Projectmgmt/taskModel';
+import { MembershipService } from 'src/app/shared/services/Membership/membership.service';
 
 @Component({
   selector: 'app-sprintactivities',
   templateUrl: './sprintactivities.html',
 })
 export class Sprintactivities implements OnInit{
-  constructor(private router:ActivatedRoute,private sprintSvc:ProjectService){}
+  constructor(private router:ActivatedRoute,private sprintSvc:ProjectService,private membershipSvc:MembershipService){}
   sprintId:number=10;
   empployeeWorks:TaskModel[]=[];
   ngOnInit(): void {
     
-      // this.sprintSvc.getSprintsTasks(this.sprintId).subscribe((res)=>{
-      //   this.empployeeWorks=res;
-      // })
+      this.sprintSvc.getSprintsTasks(this.sprintId).subscribe((res)=>{
+        this.empployeeWorks=res;
+
+        let employeeIds:number[]=this.empployeeWorks.map(a=>a.assignedTo);
+         let employeeIdsString:string =employeeIds.join(",");
+         console.log(employeeIdsString)
+         this.membershipSvc.getUserDetails(employeeIdsString).subscribe(users=>{
+          
+          this.empployeeWorks.forEach(employee=>{
+            let foundUser=users.find(user=>user.id==employee.assignedTo);
+            if(foundUser!=undefined){
+              employee.assignee=foundUser.fullName;
+              
+            }
+           
+          })
+         })
+
+      })
   }
 
 }
