c8185c0dd1db529c18ce9fcb62e4775c546e725b
sahilmankar <sahilmankar311@gmail.com>
2024-02-10 11:55:22 +0530

    models added in UI

---
+++ Solution/UISolution/TFLUIPortal/src/app/shared/Models/Projectmgmt/taskModel.ts
@@ -0,0 +1,21 @@
+export class TaskModel {
+  constructor(
+    public id: number,
+    public title: string,
+    public taskType: string,
+    public description: string,
+    public projectId: number,
+    public sprintId: number,
+    public assignedTo: number,
+    public assignedBy: number,
+    public createdOn: string,
+    public assignedOn: string,
+    public startDate: string,
+    public dueDate: string,
+    public status: string,
+    public assignee: string,
+    public assignor: string,
+    public projectTitle: string,
+    public sprintTitle: string
+  ) {}
+}
---
+++ Solution/UISolution/TFLUIPortal/src/app/shared/Models/UserMgmt/credential.ts
@@ -0,0 +1,11 @@
+ 
+
+  export class Credential {
+    constructor(
+      public contactNumber: string,
+      public password: string,
+      public lob: string
+    ) {}
+  }
+  
+ 
