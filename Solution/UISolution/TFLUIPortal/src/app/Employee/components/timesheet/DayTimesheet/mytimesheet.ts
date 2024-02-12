bc171e7c1b4173c06c53e0354a2541126498e4e6
sahilmankar <sahilmankar311@gmail.com>
2024-02-12 09:54:37 +0530

    timesheet components updated

--- Solution/UISolution/TFLUIPortal/src/app/Employee/components/timesheet/DayTimesheet/mytimesheet.ts
+++ Solution/UISolution/TFLUIPortal/src/app/Employee/components/timesheet/DayTimesheet/mytimesheet.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Timesheet } from 'src/app/shared/Entities/Timesheetmgmt/timesheet';
 import { TimesheetEntry } from 'src/app/shared/Entities/Timesheetmgmt/timesheetEntry';
+import { TimesheetEntryModel } from 'src/app/shared/Models/Timesheetmgmt/TimesheetEntryModel';
+import { TasksManagementService } from 'src/app/shared/services/TaskMgmt/tasks-management.service';
 import { TimesheetService } from 'src/app/shared/services/Timesheet/timesheet.service';
 
 @Component({
@@ -8,30 +10,50 @@ import { TimesheetService } from 'src/app/shared/services/Timesheet/timesheet.se
   templateUrl: './mytimesheet.html',
 })
 export class MyTimesheet {
-  timesheet: Timesheet={
+  timesheet: Timesheet = {
     id: 0,
     status: '',
     createdOn: '',
     modifiedOn: '',
     createdBy: 0,
-    totalHours: 0
+    totalHours: 0,
   };
   timesheetId: number = 0;
 
-  timesheetEntries: TimesheetEntry[] = [];
+  timesheetEntries: TimesheetEntryModel[] = [];
 
   constructor(
     private timesheetService: TimesheetService,
+    private taskSvc: TasksManagementService
   ) {}
 
   ngOnInit(): void {
     this.timesheetId = 3;
-    this.timesheetService.getTimeSheetById(this.timesheetId).subscribe((res) => {
-      this.timesheet = res;
-    });
-    this.timesheetService.getEntriesOfTimesheet(this.timesheetId).subscribe((res) => {
-      this.timesheetEntries = res;
-    });
-    
+    this.timesheetService
+      .getTimeSheetById(this.timesheetId)
+      .subscribe((timesheet) => {
+        this.timesheet = timesheet;
+      });
+    this.timesheetService
+      .getEntriesOfTimesheet(this.timesheetId)
+      .subscribe((timesheetEntries) => {
+        timesheetEntries.forEach((entry) => {
+          let timesheetEntry: TimesheetEntryModel = {
+            id: entry.id,
+            taskId: entry.taskId,
+            fromTime: entry.fromTime,
+            toTime: entry.toTime,
+            timesheetId: entry.timesheetId,
+            hours: entry.hours,
+            taskTitle: '',
+          };
+          this.timesheetEntries.push(timesheetEntry);
+        });
+        this.timesheetEntries.forEach((entry) =>
+          this.taskSvc.getTaskDetails(entry.taskId).subscribe((task) => {
+            entry.taskTitle = task.title;
+          })
+        );
+      });
   }
 }
