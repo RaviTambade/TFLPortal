12f1b01455e87129b1060e2a8119b448eb0971ee
Ajinkya <ajinkyatambade143@gmail.com>
2024-05-05 18:29:04 +0530

    TFLPortal Project PayRollMgmt API Modified

---
+++ Solution/WebSolution/Monolithic/Repositories/PayrollMgmt/Analytics/IPayrollAnalyticsRepository.cs
@@ -0,0 +1,15 @@
+using TFLPortal.Models;
+using TFLPortal.Responses;
+
+namespace TFLPortal.Repositories.PayrollMgmt.Analytics;
+
+
+public interface IPayrollAnalyticsRepository
+{
+    Task<List<SalarySlip>> GetPaidSalaries(int month,int year);
+    Task<List<SalarySlip>> GetSalaries(int employeeId);
+    Task<SalarySlip> GetSalary(int salaryId);
+    Task<SalaryStructure> GetSalaryStructure(int employeeId);
+    Task<PaySlip> GetSalary(int employeeId,int month,int year);
+  
+}   
